a7ecd62a90c8abe9e1b976f46ae8e51cd6d6d5c8
Will Traweek <will.traweek@gmail.com>
2020-09-28 02:34:45 -0500

    add the CodeLighthouse SDK

---
+++ sdk/CodeLighthouseWebHandler.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+class CodeLighthouseWebHandler:
+    BASE_URL = "https://dev.slackbot.mailreaper.com"
+    workspace_name = "\""
+    x_api_key = "\""
+
+    @staticmethod
+    def send_error(title: str, description: str, author: str) -> None:
+        headers = {
+            "x-api-key": CodeLighthouseWebHandler.x_api_key,
+            "Content-Type": "application/json",
+            "organization": "mailreaper"
+        }
+
+        data = {
+            "author": author,
+            "error_title": title,
+            "error": description,
+        }
+
+        url = CodeLighthouseWebHandler.BASE_URL + "/error"
+
+        prepared = requests.Request("POST", url, headers=headers, data=json.dumps(data))
+        prepared = prepared.prepare()
+        s = requests.Session()
+        r = s.send(prepared)
