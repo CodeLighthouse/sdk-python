88866f0dae314214650ddaf5e6756731485c7b75
Will Traweek <will.traweek@gmail.com>
2020-10-15 20:16:20 -0500

    add arguments tracking

--- codelighthouse/CodeLighthouse.py
+++ codelighthouse/CodeLighthouse.py
@@ -17,10 +17,30 @@ class CodeLighthouse(ContextDecorator):
                 try:
                     return f(*args, **kw)
                 except BaseException as e:
+                    arguments = CodeLighthouse.format_arguments(args, kw)
                     CodeLighthouseWebHandler.send_error(title=f"{type(e).__name__} @ {f.__name__}",
                                                         description=str(e),
-                                                        email=email)
+                                                        email=email,
+                                                        arguments=arguments)
 
             return _wrapper_inner
 
         return _wrapper_outer
+
+    @staticmethod
+    def format_arguments(args, kwargs):
+        """
+        Formats the arguments before sending them to the CodeLighthouse Backend
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        output = {"args": []}
+
+        for arg in args:
+            output["args"].append(arg)
+        for key, value in kwargs.items():
+            output[key] = value
+
+        return output
