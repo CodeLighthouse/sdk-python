42825829e4ae1ea73a1760022edbb2b065d6b477
Kyle Mistele <kyle@mistele.com>
2020-11-10 17:50:41 -0600

    Fixed way errors are printed

--- codelighthouse/CodeLighthouseWebHandler.py
+++ codelighthouse/CodeLighthouseWebHandler.py
@@ -26,7 +26,8 @@ class CodeLighthouseWebHandler:
         if r.status_code != 200 or self.DEBUG:
             # integrate logger in the future
             try:
-                print(f"CODELIGHTHOUSE: returned status code {r.status_code} || {r.json()['message']}")
+                print(f"CODELIGHTHOUSE: returned status code {r.status_code}")
+                print(f'CODELIGHTHOUSE: returned message {r.json()["message"]}')
             except json.decoder.JSONDecodeError as e:
                 if self.DEBUG:
                     print(f"JSON ERROR {e}")
